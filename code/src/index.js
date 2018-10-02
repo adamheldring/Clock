de6d934a35a98c64c0c63c0629a4c4934f59a5da
A-Game <javascript@adamheldring.com>
2018-10-02 11:35:28 +0200

    Main task working clock start/stop button

--- code/src/index.js
+++ code/src/index.js
@@ -3,16 +3,59 @@ import ReactDOM from "react-dom"
 import "./index.css"
 
 class Clock extends React.Component {
+  state = {
+    date: new Date(),
+    clockRunning: false,
+    buttonText: "Loading"
+  }
+
+  componentDidMount = () => {
+    this.startClock()
+  }
+  componentWillUnmount = () => {
+    this.stopClock()
+  }
+  tick = () => {
+    this.setState({
+      date: new Date()
+    })
+  }
+  startClock = () => {
+    this.timerID = setInterval (
+      () => this.tick(),
+      1000
+    )
+    this.setState({
+      clockRunning: true,
+      buttonText: "Stop"
+    })
+  }
+  stopClock = () => {
+    clearInterval(this.timerID)
+    this.setState({
+      clockRunning: false,
+      buttonText: "Start"
+    })
+  }
+  clickStartStop = () => {
+    if (this.state.clockRunning) {
+      this.stopClock()
+    } else {
+      this.startClock()
+    }
+  }
 
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+        <button onClick={this.clickStartStop}>{this.state.buttonText}</button>
       </div>
     )
   }
 
 }
 
-ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"))
+ReactDOM.render(<Clock />,
+document.getElementById("root"))
